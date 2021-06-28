a262acf93957b4519e7ba5ece69f7e2051dc3ee3
Sandesh <sonelsandesh@gmail.com>
2021-06-28 18:25:04 +0530

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-// export const server = "https://cuvette-backend.herokuapp.com/api/v1";
-export const server = "http://localhost:8000/api/v1";
+export const server = "https://cuvette-backend.herokuapp.com/api/v1";
+// export const server = "http://localhost:8000/api/v1";
 export const signUpUrl = server + "/users/signup";
 export const signInUrl = server + "/users/login";
 export const userDetailsUrl = server + "/users";
